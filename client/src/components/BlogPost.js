9ba826c061ecf9d4b69c60f39b50d5482562d39f
parkgain <pgi082247@gmail.com>
2022-12-12 00:58:25 +0900

    fix : fontawesome 아이콘 제대로 추가함

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
 function BlogPost() {
   return (
     <>
       <div className="font">Nopainogaini</div>
-      <FontAwesomeIcon icon="fa-solid fa-moon" />
+      <FontAwesomeIcon icon={faMoon} />
     </>
   );
 }
