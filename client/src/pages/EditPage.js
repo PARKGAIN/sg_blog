94b8f8fba9f18e8f0c77e6251defdd3dc2be29ff
parkgain <pgi082247@gmail.com>
2022-12-19 03:07:54 +0900

    feat : CRUD 제대로 말고 일단 구현 완료

--- client/src/pages/EditPage.js
+++ client/src/pages/EditPage.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import Editor from "../components/Editor";
 import { Link, useParams } from "react-router-dom";
-import Btn from "../components/Btn";
+import styles from "../Btn.module.css";
 import CancelBtn from "../components/CancelBtn";
 
 function EditPage() {
   const [post, setPost] = useState("");
   const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const content = useRef("");
   const { unq } = useParams();
   const baseUrl = "http://localhost:3000";
-  //IFFE(Immediately Invoked Function Expression)
+
   const get = async () => {
     await axios
       .get(baseUrl + `/posts/manage/${unq}`)
@@ -27,12 +27,29 @@ function EditPage() {
       });
   };
 
+  const updatePosts = async () => {
+    await axios
+      .put(baseUrl + "/posts/update", {
+        unq: unq,
+        title: title,
+        content: content.current,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     get();
   }, []);
 
-  const editText = "글 수정하기";
-
+  const handleContent = (value) => {
+    content.current = value;
+    console.log(content.current);
+  };
   return (
     <div>
       <div className="ml-40 mr-40">
@@ -42,14 +59,19 @@ function EditPage() {
               className="title_input"
               key={0}
               defaultValue={post[0].title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
             />
           ))}
-          <Editor />
+          <Editor value={content} onChange={handleContent} />
         </div>
 
         <div className="flex">
           <div>
-            <Btn text={editText} />
+            <button className={styles.write_btn} onClick={updatePosts}>
+              글수정하기
+            </button>
           </div>
           <CancelBtn className="pb-5rem">
             <Link to="/">취소</Link>
@@ -59,21 +81,5 @@ function EditPage() {
     </div>
   );
 }
-
-const updatePosts = () => {
-  const baseUrl = "http://localhost:3000";
-  axios
-    .put(baseUrl + "/posts/update", {
-      unq: unq,
-      title: title,
-      content: content,
-    })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
 
 export default EditPage;
