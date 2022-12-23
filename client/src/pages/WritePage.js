1276b85a03fb5312749f04403e833b2345e8cc63
parkgain <pgi082247@gmail.com>
2022-12-23 18:51:20 +0900

    fix : 함수 닫는 중괄호 잘 못 적어서 생긴 에러 해결

--- client/src/pages/WritePage.js
+++ client/src/pages/WritePage.js
@@ -18,39 +18,37 @@ function WritePage() {
       });
       navigate(-1);
     } catch {
-      (error) => {
-        console.log(error);
-      };
+      (error) => console.log(error);
     }
-
-    const saveText = "글 저장하기";
-    const handleContent = (value) => {
-      content.current = value;
-      console.log(content.current);
-    };
-    return (
-      <div className="ml-40 mr-40">
-        <div className="pt-5rem pb-5rem ">
-          <input
-            className="title_input"
-            placeholder=" 제목을 입력하세요"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-          <Editor value={content} onChange={handleContent} />
-        </div>
-        <div className="flex">
-          <div onClick={sendPost}>
-            <PostWriteBtn text={saveText} />
-          </div>
-          <CancelBtn>
-            <Link to="/">취소</Link>
-          </CancelBtn>
-        </div>
-      </div>
-    );
   };
+
+  const saveText = "글 저장하기";
+  const handleContent = (value) => {
+    content.current = value;
+    console.log(content.current);
+  };
+  return (
+    <div className="ml-40 mr-40">
+      <div className="pt-5rem pb-5rem ">
+        <input
+          className="title_input"
+          placeholder=" 제목을 입력하세요"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <Editor value={content} onChange={handleContent} />
+      </div>
+      <div className="flex">
+        <div onClick={sendPost}>
+          <PostWriteBtn text={saveText} />
+        </div>
+        <CancelBtn>
+          <Link to="/">취소</Link>
+        </CancelBtn>
+      </div>
+    </div>
+  );
 }
 export default WritePage;
