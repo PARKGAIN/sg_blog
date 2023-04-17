2887ce67702c9353f18349d6ce74dc6de4000e63
strider <84880886+PARKGAIN@users.noreply.github.com>
2023-04-18 00:02:34 +0900

    fix : 페이징처리 이상하게 한 코드 수정

--- client/src/pages/Home/MainPage.js
+++ client/src/pages/Home/MainPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import NumOfTotalPost from "../../components/NumOfTotalPost";
@@ -6,13 +6,14 @@ import PostList from "../../components/PostList";
 import Header from "../../components/Header";
 import useAsync from "../../hooks/useAsync";
 import Darkmode from "darkmode-js";
-const getPosts = async () => {
-  const res = await axios.get("http://localhost/paginatedposts/1");
-  return res.data;
-};
 
 function MainPage() {
+  const getPosts = async () => {
+  const res = await axios.get(`http://localhost/paginatedposts/${pageNum}`);
+  return res.data;
+};
   const [state] = useAsync(getPosts, []);
+  const [pageNum,setPageNum] = useState();
   const { loading, data: posts, error } = state;
 
   if (loading) return <div>로딩중..</div>;
@@ -42,47 +43,7 @@ function MainPage() {
       <br />
       <div className="post_write_page_center">
         <button className="pagination_btn">
-          <Link to="/1">1 </Link>
-        </button>
-
-        <button className="pagination_btn">
-          <Link to="/2">2</Link>
-        </button>
-
-        <button className="pagination_btn">
-          <Link to="/3">3</Link>
-        </button>
-
-        <button className="pagination_btn">
-          <Link to="/4">4</Link>
-        </button>
-
-        <button className="pagination_btn">
-          <Link to="/5">5</Link>
-        </button>
-
-        <button className="pagination_btn">
-          <Link to="/6">6 </Link>
-        </button>
-
-        <button className="pagination_btn">
-          <Link to="/7">7</Link>
-        </button>
-
-        <button className="pagination_btn">
-          <Link to="/8">8 </Link>
-        </button>
-
-        <button className="pagination_btn">
-          <Link to="/9">9 </Link>
-        </button>
-
-        <button className="pagination_btn">
-          <Link to="/10">10 </Link>
-        </button>
-
-        <button className="pagination_btn">
-          <Link to="/11">&gt; </Link>
+          <Link to=`/${pageNum}`>{pageNum}</Link>
         </button>
       </div>
       <br />
